79e2e0af377789a008a12e45cf6fc2eff3511c65
shishiteng <shishiteng@126.com>
2018-05-31 14:31:38 +0800

    upload visensor

--- sensors_ros/visensor7251/src/compile.sh
+++
@@ -1,1 +0,0 @@
-g++ device.cpp test.cpp -I. -I../include -I/home/vip/work/cyusb/cyusb_linux_1.0.4/include -o test -I /usr/include  -L /usr/lib -lopencv_core -lopencv_highgui -lopencv_imgproc -lpthread -lcyusb
